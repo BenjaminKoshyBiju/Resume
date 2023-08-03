eeb35c9dbac27e1ecfdf6d46fc2fa0da6b9dfdb5
BenjaminKoshyBiju <bennn455@gmail.com>
2023-08-03 18:14:17 +0530

    working on making it dynamic

--- resume/main/models.py
+++ resume/main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+
+class UserDetails(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    user = models.ForeignKey(User,default=None, on_delete=models.CASCADE)
+    address =  models.CharField(max_length=150)
+    email = models.EmailField(max_length=254)
+    interests = models.CharField(max_length=500)
+
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100)
+    companyName = models.CharField(max_length=150)
+    data= models.CharField(max_length=450)
+    user = models.ForeignKey(User,default=None, on_delete=models.CASCADE)
+
+
+class Projects(models.Model):
+    heading = models.CharField(max_length=100)
+    img=models.ImageField(upload_to='img', null=True)
+    desc= models.CharField(max_length=450)
+    user = models.ForeignKey(User,default=None, on_delete=models.CASCADE)
+
+class Education(models.Model):
+    collegeName = models.CharField(max_length=100)
+    date = models.DateField ("Date")
+    Degree= models.CharField(max_length=450)
+    user = models.ForeignKey(User,default=None, on_delete=models.CASCADE)
+
--- resume/main/views.py
+++ resume/main/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from .models import UserDetails
 # Create your views here.
-def resumeDetail(request):
 
-    
-    return render(request,'index.html')
+
+# UserDetails = UserDetails.objects.all()
+@login_required
+def resumeDetail(request,pk):
+  #  UserDetails = UserDetails.objects.filter(user_id=request.user.id)
+    UserDetails =UserDetails.objects.get(id=pk)
+    return render(request,'index.html',{'userDetails': UserDetails})
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('Detail')  
+        else:
+           
+            return render(request, 'login.html', {'error_message': 'Invalid credentials'})
+    return render(request, 'login.html')
+
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login') 
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
