09368d1590a308ad723b95e66e48bf75b989d382
BenjaminKoshyBiju <bennn455@gmail.com>
2023-08-15 01:11:32 +0530

    fully connected the backend to frontend
    everything works but the ui sucks, lol doesnt matter can make edits in the future espicialy for the update button

--- resume/main/views.py
+++ resume/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,redirect
+from django.shortcuts import render,redirect
 from .models import UserDetails,Education,Experience,Projects
 from .forms import ResumeDetailsForm,EducationForm,ExperienceForm
 # Create your views here.
@@ -7,9 +7,16 @@ from .forms import ResumeDetailsForm,EducationForm,ExperienceForm
 
 def resume(request):
   #  UserDetails = UserDetails.objects.filter(user_id=request.user.id)
-   
-
-    return render(request, 'index.html', )
+   details=UserDetails.objects.all()
+   experiences=Experience.objects.all()
+   projects=Projects.objects.all()
+   eds=Education.objects.all()
+   data=({'details':details,
+         'experiences':experiences,
+         'projects':projects,
+         'eds':eds
+         })
+   return render(request, 'index.html',data )
 
 
 
@@ -54,19 +61,35 @@ def expForm(request):
             experience_to_delete = Experience.objects.get(id=delete_id)
             experience_to_delete.delete()
             return redirect('experience') 
-        #update the form
-        update_id = request.POST.get('update_id')
-        if update_id:
-            experience_to_update = Experience.objects.get(id=update_id)
-            update_form = ExperienceForm(request.POST, instance=experience_to_update)
-            if update_form.is_valid():
-                update_form.save()
-                return redirect('resume')
-         #create new experience   
+        
+        #wil use update in the form later on was facing errors    
+        # update_id = request.POST.get('update_id')
+        # title = request.POST.get('update_id')
+        # companyName = request.POST.get('update_id')
+        # data = request.POST.get('update_id')
+        # date = request.POST.get('update_id')
+
+        # # print('hello',update_id)
+        # if update_id:
+        #     experience_to_update = Experience.objects.get(id=update_id)
+        #     experience_to_update.title=title
+        #     experience_to_update.companyName=companyName
+        #     experience_to_update.data=data
+        #     experience_to_update.date=date
+        #     print()
+        #     # update_form = ExperienceForm(request.POST, instance=experience_to_update)
+        #     # print(update_id, 'works?')
+        #     # if update_form.is_valid():
+        #     #     update_form.save()
+        #     experience_to_update.save()
+                
+        #     return redirect('experience')
+        #  #create new experience
+        # else:   
         form_list = ExperienceForm(request.POST)
         if form_list.is_valid():
             form_list.save()
-            return redirect('resume')
+            return redirect('education')
         
         
        
@@ -94,18 +117,12 @@ def eduForm(request):
             education_to_delete.delete()
             return redirect('education') 
         #update the form
-        update_id = request.POST.get('update_id')
-        if update_id:
-            education_to_update = Education.objects.get(id=update_id)
-            update_form = EducationForm(request.POST, instance=education_to_update)
-            if update_form.is_valid():
-                update_form.save()
-                return redirect('resume')
-         #create new experience   
+      
+        #create new experience   
         form_list = EducationForm(request.POST)
         if form_list.is_valid():
             form_list.save()
-            return redirect('resume')
+            return redirect('project')
         
         
        
@@ -129,7 +146,6 @@ def projectForm(request):
             project_to_delete = Projects.objects.get(id=delete_id)
             project_to_delete.delete()
             return redirect('project')
-
         update_id = request.POST.get('update_id')
         if update_id:
             project = Projects.objects.get(id=update_id)
@@ -139,19 +155,15 @@ def projectForm(request):
             if img_file:
                 project.img = img_file
             project.save()
-
         new_heading = request.POST.get('new_heading')
         new_img = request.FILES.get('new_img')
         new_desc = request.POST.get('new_desc')
         if new_heading and new_desc and new_img:
             new_project = Projects(heading=new_heading, img=new_img, desc=new_desc)
             new_project.save()
-
         return redirect('resume')
-
     projects = Projects.objects.all()
     return render(request, 'projects.html', {'projects': projects})
-
 
 
 
