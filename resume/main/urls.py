6781142d50f2e27cf77e7845b54ba973e1a7df33
BenjaminKoshyBiju <97573363+BenjaminKoshyBiju@users.noreply.github.com>
2023-09-01 08:38:33 +0530

    Update urls.py

--- resume/main/urls.py
+++ resume/main/urls.py
@@ -2,10 +2,10 @@ from .import views
 from django.urls import path
 urlpatterns = [
      path('',views.resume,name='resume'),
-     path('details/',views.resumeDetail,name='Detail'),
-     path('details/experience/',views.expForm,name='experience'),
-     path('details/education',views.eduForm,name='education'),
-     path('details/project',views.projectForm,name='project'),
+     # path('details/',views.resumeDetail,name='Detail'),
+     # path('details/experience/',views.expForm,name='experience'),
+     # path('details/education',views.eduForm,name='education'),
+     # path('details/project',views.projectForm,name='project'),
    
     
 ]
